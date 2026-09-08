1f42e05dcf5f35c796953ce5ecbe447cb6c32f90
liels <liel.rubin.77@gmail.com>
2026-09-08 14:44:34 +0300

    lllllllll

--- screen.py
+++ screen.py
@@ -1,6 +1,33 @@
 from PIL import Image
 import pygame
 import random
+import consts
 
+pygame.init()
+window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+pygame.display.set_caption(consts.NAME_GAME)
+clock = pygame.time.Clock()
+player = pygame.Rect(150, 12, 50, 50)
 
-image = pygame.image.load('snake.png')
+def draw():
+    window.fill(consts.BACKGROUND_COLOR)
+    pygame.draw.rect(window, "red", player)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_UP, pygame.K_w):
+                player.y -= consts.CELL_SIZE
+            if event.key in (pygame.K_DOWN, pygame.K_s):
+                player.y += consts.CELL_SIZE
+            if event.key in (pygame.K_LEFT, pygame.K_a):
+                player.x -= consts.CELL_SIZE
+            if event.key in (pygame.K_RIGHT, pygame.K_d):
+                player.x += consts.CELL_SIZE
+
+    draw()
+    pygame.display.update()
+    clock.tick(consts.CLOCK)
