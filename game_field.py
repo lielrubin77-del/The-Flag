1ad8d5a9bd0d1d6ec3c51dac210306f152547718
ofirbinyamin2008 <ofirbinyamin2008@gmail.com>
2026-09-08 16:43:11 +0300

    screen.py

---
+++ game_field.py
@@ -0,0 +1,17 @@
+import consts
+
+lst = []
+
+
+def list_of_status(lst):
+    for i in range(consts.BOARD_ROWS):
+        status_list = []
+        for j in range(consts.BOARD_COLS):
+            status = "empty"
+            status_list.append(status)
+        lst.append(status_list)
+
+
+list_of_status(lst)
+
+print(len(lst))
