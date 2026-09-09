ed80d6b65cb3be61b01642c10ade878365419830
ofirbinyamin2008 <ofirbinyamin2008@gmail.com>
2026-09-09 11:38:30 +0300

    successful

--- game_field.py
+++ game_field.py
@@ -1,4 +1,5 @@
 import consts
+import random
 
 lst = []
 
@@ -13,5 +14,54 @@ def list_of_status(lst):
 
 
 list_of_status(lst)
-#mdkgvmfdm
 print(len(lst))
+print(lst[consts.BOARD_ROWS- consts.FLAG_ROWS][consts.BOARD_COLS - consts.FLAG_COLS])
+
+
+def change_status_to_flag():
+    x = consts.BOARD_ROWS - consts.FLAG_ROWS  # 22
+    y = consts.BOARD_COLS - consts.FLAG_COLS  # 46
+    for i in range(consts.FLAG_ROWS):           #3
+        for j in range(consts.FLAG_COLS):       #4
+            lst[x][y] = "flag"
+            y+=1
+        y = consts.BOARD_COLS - consts.FLAG_COLS  # 46
+        x += 1
+
+change_status_to_flag()
+def print_list(lst):
+    for row in lst:
+        for col in row:
+            print (col,end=' ')
+        print()
+print_list(lst)
+
+
+
+def is_mine_placement_valid(x, y):
+    y_cord=y
+    for i in range(consts.MINE_ROWS):
+        for j in range(consts.MINE_COLS):
+            if (not (lst[x][y] == "empty")):
+                return False
+            y += 1
+        y=y_cord
+        x += 1
+
+
+def random_mine():  # הגרלת מוקש ושינוי מצב ל"מוקש" בהתאם
+    for i in range(consts.MINES_COUNT):
+        x = random.randint(0,consts.BOARD_ROWS - consts.MINE_ROWS)  # נגריל בגבולות של המוקש
+        y = random.randint(0, consts.BOARD_COLS - consts.MINE_COLS)
+        while (is_mine_placement_valid(x, y) == False):
+            x = random.randint(0,consts.BOARD_ROWS - consts.MINE_ROWS)  # נגריל בגבולות של המוקש
+            y = random.randint(0, consts.BOARD_COLS - consts.MINE_COLS)
+        y_cord=y
+        for i in range(consts.MINE_ROWS):
+            for j in range(consts.MINE_COLS):
+                lst[x][y] = "mine"
+                y+=1
+            y=y_cord
+            x += 1
+random_mine()
+print_list(lst)
